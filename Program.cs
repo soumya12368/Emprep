7c08a0aadffbe8a3f8d6cf58183a81c8aa8e6ce9
Soumya Sutar <soumsutar9@gmail.com>
2024-01-10 09:59:35 +0530

    Soumya

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConappTwo;
+
+List<Employee> employees = new List<Employee>()
+{
+    new Employee() {Id=1,Name="Vijay",Designation="Developer",Doj=new DateTime(day:12,month:11,year:2022)},
+    new Employee() {Id=2,Name="Niraj",Designation="Tester",Doj=new DateTime(day:12,month:11,year:2022)},
+    new Employee() {Id=3,Name="Vipul",Designation="Manager",Doj=new DateTime(day:12,month:11,year:2022)},
+    new Employee() {Id=4,Name="Gagan",Designation="Developer",Doj=new DateTime(day:12,month:11,year:2022)},
+
+};
+Console.WriteLine("ID\t Name \t Designation \t Date of Joining");
+foreach (Employee emp in employees)
+{
+    Console.Write(emp.Id + "\t \t");
+    Console.Write(emp.Name + "\t \t");
+    Console.Write(emp.Designation + "\t \t");
+    Console.Write(emp.Doj.ToLongDateString());
+    Console.WriteLine("\n");
+
+
+
+
+}
